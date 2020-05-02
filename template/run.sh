02ccc3ddb4e7689c02a8db6000718831745e8a00
Vitalii Burlai <vitaly.burlai@gmail.com>
2020-05-02 13:58:19 +0100

    Periodic runs

--- template/run.sh
+++ template/run.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-node solution.js <input.txt >output.txt
+LS=""
+while [ 1 ]; do
+  LS2=$( ls -l )
+  if [ "$LS2" != "$LS" ]; then
+    echo 'run'
+    node solution.js <input.txt >output.txt 2>&1;
+    LS=$( ls -l )
+  fi;
+  sleep 1s;
+done;
